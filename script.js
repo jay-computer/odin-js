c134c2a34f4453e321ad5438340ae7b73629f7f3
jay-computer <joshuakang19@gmail.com>
2022-08-08 12:51:34 -0400

    Finish form transition

--- script.js
+++ script.js
@@ -4,9 +4,6 @@ function Book(title, author, pages) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.info = function() {
-        return `${title} by ${author}, ${pages} pages`
-    }
 }
 
 function addBookToLibrary(book) {
@@ -19,23 +16,43 @@ function displayLibrary() {
     }
 }
 
-const book1 = new Book("title1", "author1", 23);
-const book2 = new Book("title2", "author2", 53);
-const book3 = new Book("title3", "author3", 93);
-
-addBookToLibrary(book1);
-addBookToLibrary(book2);
-addBookToLibrary(book3);
-
-displayLibrary();
 
 
 //Query selectors and functions to move the form up and down
 
+
+
 function translateForm() {
-    
+    submitForm.classList.add("active");
 }
 
+function parseBookInfo() {
+    //create a book
+    let title = titleInput.value;
+    let author = authorInput.value;
+    let pageNum = pageNumInput.value;
+    //create and add a book
+    const book = new Book(title, author, pageNum);
+    addBookToLibrary(book);
+    console.log(myLibrary);
+    //move form back to original place
+    submitForm.classList.remove("active");
+}
 
+function cancelForm() {
+    submitForm.classList.remove("active");
+}
+
+const body = document.querySelector("body");
+const submitForm = document.querySelector(".submit-book");
 const addBook = document.querySelector(".addBook");
+const cancelBook = submitForm.querySelector('input[name="cancel"]' );
+const addBookButton = document.querySelector('input[name="addBook"]');
+
+const titleInput = document.getElementById("title");
+const authorInput = document.getElementById("author");
+const pageNumInput = document.getElementById("pageNum");
+
+cancelBook.addEventListener("click", cancelForm);
+addBookButton.addEventListener("click", parseBookInfo );
 addBook.addEventListener("click", translateForm);
